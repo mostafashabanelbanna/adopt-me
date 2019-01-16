8bf33df56194744f0df43050daeceaac5d4f54c6
Mostafa-shaban <dashassas@gmail.com>
2019-01-16 02:12:26 +0200

    add js file

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const App = () => {
+	return React.createElement('div', {}, React.createElement('h1', {}, 'adopt-me!'))
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'))
