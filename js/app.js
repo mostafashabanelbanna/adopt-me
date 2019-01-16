bc50fb4ec0bf3eb7c335bedd71eee8fea1633870
Mostafa-shaban <dashassas@gmail.com>
2019-01-16 02:58:18 +0200

    making reusable component with passing props

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
+const Pet = (props) => {
+	return React.createElement('div', {},[
+		React.createElement('h1', {}, props.name),
+		React.createElement('h2', {}, props.animal),
+		React.createElement('h2', {}, props.breed)
+	]);
+}
 const App = () => {
-	return React.createElement('div', {}, React.createElement('h1', {}, 'adopt-me!'))
+	return React.createElement('div', {}, [
+		React.createElement('h1', {}, 'adopt-me!'),
+		React.createElement(Pet, {
+			name : 'max',
+			animal : 'dog',
+			breed : 'german'
+		}),
+		React.createElement(Pet, {
+			name : 'kitty',
+			animal : 'cat',
+			breed : 'mixed'
+		})	
+		]);
 }
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
